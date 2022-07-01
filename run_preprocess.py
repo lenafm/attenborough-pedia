a8f144ed9f5fd81b37ff6a23dabb8ef22dace8c9
Lena Mangold <lena.mangold@cmb.hu-berlin.de>
2022-07-01 12:18:56 -0600

    add script to preprocess all srt files

--- run_preprocess.py
+++ run_preprocess.py
@@ -1,23 +1,34 @@
+# import modules
+import os
+import argparse
 import pysrt
 import spacy
-from tqdm.auto import tqdm
 from copy import deepcopy
-from tabulate import tabulate
 
-from utils.preprocess_text import preprocess, token_filter
+from utils.preprocess_text import preprocess
 
-subtitles = pysrt.open("./Copy of 08. Lords of the air.srt")
-nlp = spacy.load("en_core_web_lg")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--indir', type=str,
+                        help='input directory', default='data/srt')
+    parser.add_argument('--outdir', type=str,
+                        help='output directory', default='data/preprocessed_subtitles')
+    args = parser.parse_args()
 
-utterances = [subtitle.text for subtitle in subtitles]
+    indir = args.indir
+    outdir = args.outdir
+    nlp = spacy.load("en_core_web_lg")
 
-corpus = nlp.pipe(utterances, batch_size=26000)
+    filenames = os.listdir(indir)
+    for file in filenames:
+        print('Processing file {}...'.format(file))
+        subtitles = pysrt.open(os.path.join(indir, file), encoding='latin-1')
+        utterances = [subtitle.text for subtitle in subtitles]
+        corpus = nlp.pipe(utterances, batch_size=26000)
+        processed = preprocess(corpus)
+        preprocessed_subtitles = deepcopy(subtitles)
 
-processed = preprocess(corpus)
+        for subtitle, proc_subtitle in zip(preprocessed_subtitles, processed):
+            subtitle.text = " ".join(proc_subtitle)
 
-preprocessed_subtitles = deepcopy(subtitles)
-
-for subtitle, proc_subtitle in zip(preprocessed_subtitles, processed):
-    subtitle.text = " ".join(proc_subtitle)
-
-preprocessed_subtitles.save('./preprocessed_sub.txt')
+        preprocessed_subtitles.save(os.path.join(outdir, '{}.txt'.format(file[:-4])))
